e015e3e941e58e53420940ec77f4d187df0a8cd2
Mysthios <atharhumam95@gmail.com>
2025-10-24 16:51:59 +0700

    add: routes (on going)

---
+++ lib/route/route.dart
@@ -0,0 +1,18 @@
+
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String verification = '/verification';
+  static const String homepage = '/homepage';
+
+  //---------------- PRODUCTS PAGE ----------------
+  static const String product = '/product';
+  static const String addPrice = '/product/add_price';
+
+  //---------------- TRANSACTION PAGE ----------------
+
+  //---------------- PRODUCTS PAGE ----------------
+
+  //---------------- PRODUCTS PAGE ----------------
+}
