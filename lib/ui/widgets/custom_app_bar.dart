a607d05ee3ede7b0e474ed3ba5c7d456ac96765f
Mysthios <atharhumam95@gmail.com>
2025-11-05 15:05:55 +0700

    add : custom app bar

---
+++ lib/ui/widgets/custom_app_bar.dart
@@ -0,0 +1,75 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:pos_mobile/core/theme/theme.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final List<String>? tabs;
+  const CustomAppBar({super.key, required this.title, this.tabs});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: primaryGreenColor,
+      foregroundColor: Colors.white,
+      elevation: 0,
+      titleSpacing: 16,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: AutoSizeText(
+              title,
+              style: const TextStyle(
+                fontSize: 22, // font maksimal
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Segoe',
+              ),
+              maxLines: 1,
+              minFontSize: 12,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          const SizedBox(width: 8),
+          const Text(
+            'Auro Pay',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Segoe',
+            ),
+          ),
+        ],
+      ),
+      bottom: tabs != null
+          ? TabBar(
+        tabs: tabs!
+            .map(
+              (t) => Tab(
+            text: t,
+          ),
+        )
+            .toList(),
+        labelColor: Colors.white,
+        unselectedLabelColor: Colors.white70,
+        indicatorColor: Colors.white,
+        indicatorWeight: 3,
+        labelStyle: const TextStyle(
+          fontFamily: 'Segoe',
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          fontFamily: 'Segoe',
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+      )
+          : null,
+    );
+  }
+
+  @override
+  Size get preferredSize =>
+      Size.fromHeight(tabs != null ? 112 : 66);
+}
