f506555bf65070fccf245b5073b6e9b63ee35323
Mysthios <atharhumam95@gmail.com>
2025-11-05 15:13:36 +0700

    add : add theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const primaryGreenColor = Color.fromRGBO(57, 180, 72, 1.0);
+const primaryGreenColor = Color.fromRGBO(2, 173, 137, 1.0);
 const primaryBlueColor = Color.fromRGBO(34, 102, 174, 1.0);
 const secondaryGreenColor = Color.fromRGBO(57, 180, 72, 1.0);
 const tertiaryBlueColor = Color.fromRGBO(36, 40, 69, 1.0);
+
+const fontType = 'Segoe';
