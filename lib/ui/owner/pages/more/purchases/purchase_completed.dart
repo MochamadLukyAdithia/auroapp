8f46342b789bc4ba855da84224c271858a8ad9ee
Mysthios <atharhumam95@gmail.com>
2025-11-05 15:10:51 +0700

    add : purchase complete

---
+++ lib/ui/owner/pages/more/purchases/purchase_completed.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+// import '../../../../../core/theme/theme.dart';
+//
+//
+// class PurchaseCompleted extends StatelessWidget {
+//   const PurchaseCompleted({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('purchase page'),
+//         backgroundColor: primaryGreenColor,
+//         foregroundColor: Colors.white,
+//       ),
+//     );
+//   }
+// }
